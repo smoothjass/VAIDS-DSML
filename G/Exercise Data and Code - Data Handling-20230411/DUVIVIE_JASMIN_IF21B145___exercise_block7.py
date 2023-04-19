a9e26fe1c8ded90aaaaa9293b127a3e9309c3a17
smoothjass <100789381+smoothjass@users.noreply.github.com>
2023-04-19 19:10:10 +0200

    exercise 7 finished except for step 7 (optional)

--- G/Exercise Data and Code - Data Handling-20230411/DUVIVIE_JASMIN_IF21B145___exercise_block7.py
+++ G/Exercise Data and Code - Data Handling-20230411/DUVIVIE_JASMIN_IF21B145___exercise_block7.py
@@ -45,6 +45,11 @@ the dataset includes all kinds of errors
     - use the groupby() method
 '''
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.impute import KNNImputer
+from sklearn.datasets import load_wine
 
 ########################################################################################################################
 # Solution
@@ -57,12 +62,10 @@ PLease note:
 '''
 
 # set pandas options to make sure you see all info when printing dfs
-import pandas as pd
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
-
 
 # ----------------------------------------------------------------------------------------------------------------------
 # >>> step 1, just try it
@@ -70,7 +73,7 @@ pd.set_option('display.max_colwidth', None)
 '''
 start by just loading the data
 '''
-df = pd.read_csv('wine_exercise.csv', sep=';')
+# df = pd.read_csv('wine_exercise.csv', sep=';')
 
 # ----------------------------------------------------------------------------------------------------------------------
 # >>> step 2, use skip rows
@@ -78,7 +81,7 @@ df = pd.read_csv('wine_exercise.csv', sep=';')
 '''
 skip row to ignore text inside the file
 '''
-df = pd.read_csv('wine_exercise.csv', sep=';', skiprows=1)
+# df = pd.read_csv('wine_exercise.csv', sep=';', skiprows=1)
 
 # ----------------------------------------------------------------------------------------------------------------------
 # >>> step 3, use skipfooter
@@ -86,8 +89,8 @@ df = pd.read_csv('wine_exercise.csv', sep=';', skiprows=1)
 '''
 the footer is also a problem, skip it with skipfooter
 '''
-df = pd.read_csv('wine_exercise.csv', sep=';', skiprows=1, skipfooter=1, engine='python')
-df.info()
+# df = pd.read_csv('wine_exercise.csv', sep=';', skiprows=1, skipfooter=1, engine='python')
+# df.info()
 # ----------------------------------------------------------------------------------------------------------------------
 # >>> step 4, check na, data types
 # ----------------------------------------------------------------------------------------------------------------------
@@ -109,10 +112,34 @@ displayed as a category with df.info() but many are object when they should be f
 hint: rows 50, 51, 142 are problematic due to mixed/wrong separators or wrong commas
 How could you find such issues in an automated way?
 '''
-#df = pd.read_csv('wine_exercise.csv', sep=';', skiprows=1, skipfooter=1, engine='python')
-#df.astype({'alcohol': 'float64'}).dtypes
-#TODO automate
+# df = pd.read_csv('wine_exercise.csv', sep=';', skiprows=1, skipfooter=1, engine='python')
+# df.astype({'alcohol': 'float64'}).dtypes
+# the types can't be converted because some cells are messed up.
 
+# exclude messed up lines manually
+# df = pd.read_csv('wine_exercise.csv', sep=';', decimal='.', skiprows=[0, 52, 53, 144, 168], skipfooter=1, engine='python', na_values='missing')
+
+# or automatically
+'''
+This just checks if commas or white spaces are found. this could be improved by looking for a list of
+wanted or unwanted separators but I am lazy.
+'''
+file = open('wine_exercise.csv', 'r')
+counter = 0
+# these row indices will be skipped
+skip_lines = []
+# this is the content to later be fixed
+repair_lines = []
+for line in file:
+    if line.find(",") != -1 or line.find(" ") != -1:
+        skip_lines.append(counter)
+        repair_lines.append(line)
+    counter += 1
+file.close()
+df = pd.read_csv('wine_exercise.csv', sep=';', decimal='.', skiprows=skip_lines, engine='python', na_values='missing')
+df.astype({'alcohol': 'float64', 'ash': 'float64'}).dtypes
+# now the conversion works
+# df.info()
 # ----------------------------------------------------------------------------------------------------------------------
 # >>> step 6, exclude the three problematic rows
 # ----------------------------------------------------------------------------------------------------------------------
@@ -120,11 +147,7 @@ How could you find such issues in an automated way?
 the three rows are completely ruined and can only be fixed in isolation
 you can read the dataset an skip these rows
 '''
-#and also 166 because there is a white space instead of a ';' as a separator
-df = pd.read_csv('wine_exercise.csv', sep=';', decimal='.', skiprows=[0, 52, 53, 144, 168], skipfooter=1, engine='python',
-                 na_values='missing')
-df.astype({'alcohol': 'float64', 'ash': 'float64'}).dtypes
-df.info()
+# as done above
 
 # ----------------------------------------------------------------------------------------------------------------------
 # step 7, handle rows separately
@@ -147,8 +170,6 @@ columns = df.columns.values.tolist()
 read only row 52 and repair it in isolation
 write it to disk wit correct separators, decimals
 '''
-row = pd.read_csv('wine_exercise.csv', sep=",", skiprows = lambda x: x not in [52], names=columns)
-string = row.to_csv(index=0, header=None, sep=";").strip('\n').split('\n')
 
 # ----------------------------------------------------------------------------------------------------------------------
 # step 7.3, handle row 53
@@ -172,8 +193,7 @@ now re read all data (4 dataframes - the original one without rows51, 52, 144 an
 combine the three dataframes and recheck for data types (try to convert numeric attributes into float - you'll see the problems then
 If you have skipped the three ruined rows just read the df without the three ruined rows and continue to check dtypes
 '''
-
-
+# df.info()
 # ----------------------------------------------------------------------------------------------------------------------
 # step 8, handle categorical data
 # ----------------------------------------------------------------------------------------------------------------------
@@ -182,10 +202,13 @@ now you can look at unique values of categorical attributes using e.g. value_cou
 this way you'll find problematic values that need recoding (e.g. AUT to AUTUMN)
 Here you can also check if there is a column in which two columns are combined and split it
 '''
-print(df['season'].value_counts())
+# print(df['season'].value_counts())
 df.replace('spring', 'SPRING', inplace=True)
 df.replace('aut', 'AUTUMN', inplace=True)
-print(df['season'].value_counts())
+# print(df['season'].value_counts())
+
+# print(df['color'].value_counts())
+# if there are only two colors, do i need to split the column to is_1 or is_0 or do i leave it that way?
 
 # ----------------------------------------------------------------------------------------------------------------------
 # step 9, check split columns
@@ -193,13 +216,16 @@ print(df['season'].value_counts())
 '''
 data type changes might be needed for split columns
 '''
+df[['country', 'years-old']] = df.loc[:, 'country-age'].str.split("-", expand=True)
+df['years-old'] = df.loc[:, 'years-old'].str.replace("years", "")
+df.drop('country-age', axis=1, inplace=True)
+df['years-old'] = df['years-old'].astype('int')
 
-
+# df.info()
 # ----------------------------------------------------------------------------------------------------------------------
 # step 10, exclude duplicates
 # ----------------------------------------------------------------------------------------------------------------------
-
-
+df.drop_duplicates(inplace=True)
 
 # ----------------------------------------------------------------------------------------------------------------------
 # step 11, find outliers
@@ -209,7 +235,57 @@ try to use plots to find outliers "visually"
 you can also try to use statistical measures to automatically exclude problematic values but be careful
 '''
 
+columns = df.select_dtypes(include=np.number).columns.values.tolist()
+columns.remove("target")
 
+scaler = MinMaxScaler()
+df[columns] = scaler.fit_transform(df[columns])
+
+boxplot = df.boxplot(column=columns)
+plt.xticks(rotation=-90)
+plt.subplots_adjust(bottom=0.4)
+plt.show()
+'''
+for label in columns:
+    boxplot = df.boxplot(column=label)
+    plt.show()
+'''
+
+'''
+these look suspicious:
+    malic_acid
+    total_phenols
+    
+these have some outliers but don't look too weird, i'd say:
+    ash
+    alcalinity_of_ash
+    magnesium
+    proanthocyanins
+    color_intensity
+    hue
+'''
+boxplot = df.boxplot(column=['malic_acid'])
+plt.show()
+boxplot = df.boxplot(column=['total_phenols'])
+plt.show()
+
+# malic_acid: it seems like missing values are encoded as 0 (or -999 before scaling)
+df['malic_acid'] = df.loc[:, 'malic_acid'].replace(0, np.nan)
+boxplot = df.boxplot(column=['malic_acid'])
+plt.show()
+# much better
+
+# the proline boxplot doesn't look to weird, but when looking at the dataframe the zero values seem off
+# it also says in the instructions that the proline nan values are to be considered
+df['proline'] = df.loc[:, 'proline'].replace(0, np.nan)
+
+# total_phenols: two values (163, 164) seem to have the comma in the wrong place
+# inverse scaling to find wrong phenols
+df[columns] = scaler.inverse_transform(df[columns])
+# find the wrong phenols
+fix_phenols = df.query('total_phenols > 100')
+# replace in old df with scaled values
+df.loc[df.total_phenols.isin(fix_phenols.total_phenols), ['total_phenols']] = fix_phenols[['total_phenols']] / 100
 
 # ----------------------------------------------------------------------------------------------------------------------
 # step 12, impute values
@@ -217,7 +293,10 @@ you can also try to use statistical measures to automatically exclude problemati
 '''
 impute missing values and excluded values using the KNN-Imputer
 '''
-
+original = load_wine()
+# print(original['feature_names'])
+imputer = KNNImputer(n_neighbors=5)
+df[original['feature_names']] = imputer.fit_transform(df[original['feature_names']])
 
 # ----------------------------------------------------------------------------------------------------------------------
 # step 13, some more info on the ds
@@ -225,8 +304,6 @@ impute missing values and excluded values using the KNN-Imputer
 '''
 get the class distribution of the target variable
 '''
+class_balancing = df[["target"]].groupby(["target"]).size()
 
-
-
-
-
+# maybe 7
---
+++ G/Exercise Data and Code - Data Handling-20230411/exercise.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+f = open('wine_exercise.csv', 'r')
+
+print(type(f.readline()))
+
+f.close()
