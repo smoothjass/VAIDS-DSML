03b6781c8abba0a8cd17ed791c9a4d6364bbb84a
smoothjass <100789381+smoothjass@users.noreply.github.com>
2023-05-09 23:06:00 +0200

    k-means exercise_block10_kmeans.py started

--- J/Exercise Data and Code - Clustering - k means-20230505/exercise_block10_kmeans.py
+++ J/Exercise Data and Code - Clustering - k means-20230505/exercise_block10_kmeans.py
@@ -23,7 +23,7 @@
 - when you open exercise files you'll see error (e.g. unassigned variables)
 - this is due to the fact that some parts are missing, and you should fill them in
 '''
-
+import math
 
 ########################################################################################################################
 # IMPORTS
@@ -35,7 +35,6 @@ import seaborn as sns
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
-
 
 ########################################################################################################################
 # PART 1 // IMPLEMENT k-Means
@@ -63,7 +62,6 @@ visualize the cluster updates:
 - show how cluster centers moves over multiple iteration
 '''
 
-
 # load data #
 # -- pre-coded -- #
 pth = r"ccc.csv"
@@ -71,8 +69,8 @@ df = pd.read_csv(pth)
 df.describe()
 df.dtypes
 df.isna().sum()
-df.drop(["Sl_No", "Customer Key"], axis=1, inplace=True) # drop keys
-df = df.astype("float64") # we will need float values for k-means
+df.drop(["Sl_No", "Customer Key"], axis=1, inplace=True)  # drop keys
+df = df.astype("float64")  # we will need float values for k-means
 df.dtypes
 
 # look at the data (pair plots) #
@@ -90,21 +88,53 @@ plt.show()
 
 # implement k-means #
 # -- student work -- #
-m = np.array([[2.5, 100000],[10.0,125000]]) # we use two cluster centers
+# [total_visits_online, avg_credit_limit] #
+m = np.array([[2.5, 100000], [10.0, 125000]])  # we use two cluster centers
 
-# scale the data first and afterwards m ! (So that that m does not jump to extreme values) 
+
+# scale the data first and afterwards m ! (So that that m does not jump to extreme values)
+scaler = MinMaxScaler()
+df['Total_visits_online'] = scaler.fit_transform(df['Total_visits_online'].values.reshape(-1, 1))
+m[:, 0] = scaler.transform(m[:, 0].reshape(-1, 1))[:, 0]
+df['Avg_Credit_Limit'] = scaler.fit_transform(df['Avg_Credit_Limit'].values.reshape(-1, 1))
+m[:, 1] = scaler.transform(m[:, 1].reshape(-1, 1))[:, 0]
+
 
 # k-means #1 ... distance and cluster assignment
+def clustering(df, m):
+    for index in df.index:
+        first_dist = math.sqrt(
+            (m[0][0] - df['Total_visits_online'][index]) ** 2 + (m[0][1] - df['Avg_Credit_Limit'][index]) ** 2)
+        second_dist = math.sqrt(
+            (m[1][0] - df['Total_visits_online'][index]) ** 2 + (m[1][1] - df['Avg_Credit_Limit'][index]) ** 2)
+        if first_dist < second_dist:
+            df.loc[index, 'cluster'] = 0
+        else:
+            df.loc[index, 'cluster'] = 1
+
 
 # k-means #2 ... means update
+def means_update(df, m):
+    cluster = df.query('cluster == 0')
+    m[0][0] = cluster.loc[:, 'Total_visits_online'].mean()
+    m[0][1] = cluster.loc[:, 'Avg_Credit_Limit'].mean()
+    cluster = df.query('cluster == 1')
+    m[1][0] = cluster.loc[:, 'Total_visits_online'].mean()
+    m[1][1] = cluster.loc[:, 'Avg_Credit_Limit'].mean()
+    return m
+
 
 # visualize results
+def visualize(df, m):
+    sns.jointplot(data=df, x="Total_visits_online", y="Avg_Credit_Limit", kind="hist", hue='cluster')
+    plt.scatter(m[0][0], m[0, 1], color='darkslateblue')
+    plt.scatter(m[1][0], m[1, 1], color='firebrick')
+    plt.show()
+
 
 # iterate
+df = df.assign(cluster=np.zeros((660,), dtype=int))
 
-
-
-
-
-
-
+clustering(df, m)
+visualize(df, m)
+m = means_update(df, m)
